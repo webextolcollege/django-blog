d937fb45637ed86af868a2d668f8124334ad493c
webextol <webextolcollege@gmail.com>
2025-09-21 01:53:51 +0500

    final deployment

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -124,14 +124,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-# STATIC_URL = 'static/'
-# STATIC_ROOT = BASE_DIR / 'static'
-# STATICFILES_DIRS = [
-#     'blog_main/static/',
-# ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-
-
+STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = [
+    'blog_main/static',
+]
 
 
 # Default primary key field type
@@ -139,9 +136,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_URL = 'static/'
+
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / 'media'
 
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
